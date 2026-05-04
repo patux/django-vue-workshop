2c20e238daefd255af52a87480d6e878b3954197
Geronimo Orozco <188718+patux@users.noreply.github.co>
2026-05-04 00:05:57 -0600

    fix: remove pip3 user msg, and unnecesary \n

--- backend.sh
+++ backend.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 dnf install tar curl dnf-plugins-core -y
 dnf install python3 python3-pip -y
-sudo -u vagrant python3 -m pip install  virtualenv
+sudo -u vagrant python3 -m pip install virtualenv --no-warn-script-location
--- common.sh
+++ common.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 dnf install net-tools git -y
 sudo -u vagrant mkdir -p /vagrant/workshop
-echo -e "\nLogged to $(hostname) !!\nThe work environment is in /vagrant/workshop\n\n" > /etc/motd
+echo -e "Logged to $(hostname) !!\nThe work environment is in /vagrant/workshop\n" >/etc/motd
